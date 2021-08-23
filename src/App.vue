f07e48566a6b3a371de3128f1bae929d9b15e39e
“Pavel <itspavelnosarev@gmail.com>
2021-08-23 16:47:55 -0400

    added functionality on database and fixed edit page

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <header class="header-area header-padding-2 sticky-bar header-res-padding clearfix">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-xl-9 col-lg-2 col-md-6 col-4">
-            <div class="logo">
-              <a href="/">
-                <img alt="" src="assets/img/logo/logo.png" />
-              </a>
-            </div>
+          <!-- <div class="col-xl-9 col-lg-2 col-md-6 col-4"> -->
+          <div class="logo">
+            <a href="/">
+              <img alt="" src="assets/img/logo/logo.png" class="logo" />
+            </a>
           </div>
+          <!-- </div> -->
           <div class="col-xl-8 col-lg-8 d-none d-lg-block">
             <div class="main-menu">
               <nav>
@@ -17,92 +17,13 @@
                   <li>
                     <a href="/">
                       Home
-                      <i class="fa fa-angle-down"></i>
+                      <!-- <i class="fa fa-angle-down"></i> -->
                     </a>
-                    <ul class="mega-menu mega-menu-padding">
-                      <li>
-                        <ul>
-                          <li class="mega-menu-title"><a href="#">Demo Group 01</a></li>
-                          <li><a href="index.html">Home 1 – Fashion</a></li>
-                          <li><a href="index-2.html">Home 2 – Fashion</a></li>
-                          <li><a href="index-3.html">Home 3 – Fashion</a></li>
-                          <li><a href="index-4.html">Home 4 – Fashion</a></li>
-                          <li><a href="index-5.html">Home 5 – Fashion</a></li>
-                          <li><a href="index-6.html">Home 6 – Fashion</a></li>
-                          <li><a href="index-7.html">Home 7 – Fashion</a></li>
-                          <li><a href="index-8.html">Home 8 – Minimal</a></li>
-                          <li><a href="index-9.html">Home 9 – Electronics</a></li>
-                          <li><a href="index-10.html">Home 10 – Furniture</a></li>
-                          <li><a href="index-11.html">Home 11 - showcase slider</a></li>
-                        </ul>
-                      </li>
-                      <li>
-                        <ul>
-                          <li class="mega-menu-title"><a href="#">Demo Group 02</a></li>
-                          <li><a href="index-12.html">Home 12 – Plants</a></li>
-                          <li><a href="index-13.html">Home 13 – Cosmetic</a></li>
-                          <li><a href="index-14.html">Home 14 – Christmas</a></li>
-                          <li><a href="index-15.html">Home 15 – Fruit</a></li>
-                          <li><a href="index-16.html">Home 16 – Black Friday</a></li>
-                          <li><a href="index-17.html">Home 17 – Flower</a></li>
-                          <li><a href="index-18.html">Home 18 – Book</a></li>
-                          <li><a href="index-19.html">Home 19 – Fashion</a></li>
-                          <li><a href="index-20.html">Home 20 – Electronics</a></li>
-                          <li><a href="index-21.html">Home 21 – Furniture</a></li>
-                          <li><a href="index-22.html">Home 22 – Handmade</a></li>
-                        </ul>
-                      </li>
-                      <li>
-                        <ul>
-                          <li class="mega-menu-title"><a href="#">Demo Group 03</a></li>
-                          <li><a href="index-23.html">Home 23 – Organic</a></li>
-                          <li><a href="index-24.html">Home 24 – Pet Food</a></li>
-                          <li><a href="index-25.html">Home 25 – Auto Parts</a></li>
-                          <li><a href="index-26.html">Home 26 – Cake Shop</a></li>
-                          <li><a href="index-27.html">Home 27 – Kids Fashion</a></li>
-                          <li><a href="index-28.html">Home 28 – Book Shop</a></li>
-                          <li><a href="index-29.html">Home 29 – Flower Shop</a></li>
-                          <li><a href="index-30.html">Home 30 – Instagram</a></li>
-                          <li><a href="index-31.html">Home 31 – Black Friday</a></li>
-                          <li><a href="index-32.html">Home 32 – Valentine Day</a></li>
-                          <li><a href="index-33.html">Home 33 – Medical Equipment</a></li>
-                        </ul>
-                      </li>
-                    </ul>
                   </li>
                   <li>
-                    <a href="shop.html">
-                      Shop
-                      <i class="fa fa-angle-down"></i>
-                    </a>
                     <ul class="mega-menu">
-                      <li>
-                        <ul>
-                          <li class="mega-menu-title"><a href="#">shop layout</a></li>
-                          <li><a href="shop.html">standard style</a></li>
-                          <li><a href="shop-filter.html">Grid filter style</a></li>
-                          <li><a href="shop-grid-2-col.html">Grid 2 column</a></li>
-                          <li><a href="shop-no-sidebar.html">Grid No sidebar</a></li>
-                          <li><a href="shop-grid-fw.html">Grid full wide</a></li>
-                          <li><a href="shop-right-sidebar.html">Grid right sidebar</a></li>
-                          <li><a href="shop-list.html">list 1 column box</a></li>
-                          <li><a href="shop-list-fw.html">list 1 column full wide</a></li>
-                          <li><a href="shop-list-fw-2col.html">list 2 column full wide</a></li>
-                        </ul>
-                      </li>
-                      <li>
-                        <ul>
-                          <li class="mega-menu-title"><a href="#">product details</a></li>
-                          <li><a href="product-details.html">tab style 1</a></li>
-                          <li><a href="product-details-2.html">tab style 2</a></li>
-                          <li><a href="product-details-3.html">tab style 3</a></li>
-                          <li><a href="product-details-4.html">sticky style</a></li>
-                          <li><a href="product-details-5.html">gallery style</a></li>
-                          <li><a href="product-details-slider-box.html">Slider style</a></li>
-                          <li><a href="product-details-affiliate.html">affiliate style</a></li>
-                          <li><a href="product-details-6.html">fixed image style</a></li>
-                        </ul>
-                      </li>
+                      <li></li>
+                      <li></li>
                       <li>
                         <ul>
                           <li class="mega-menu-img">
@@ -116,9 +37,9 @@
                   <li>
                     <a href="/usercoins">
                       My Coins
-                      <i class="fa fa-angle-down"></i>
+                      <!-- <i class="fa fa-angle-down"></i> -->
                     </a>
-                    <ul class="submenu">
+                    <!-- <ul class="submenu">
                       <li><a href="/about">about us</a></li>
                       <li><a href="cart-page.html">cart page</a></li>
                       <li><a href="checkout.html">checkout</a></li>
@@ -127,24 +48,12 @@
                       <li><a href="/">login</a></li>
                       <li><a href="contact.html">contact us</a></li>
                       <li><a href="404.html">404 page</a></li>
-                    </ul>
+                    </ul> -->
                   </li>
                   <li>
-                    <a href="#">
-                      Blog
-                      <i class="fa fa-angle-down"></i>
-                    </a>
-                    <ul class="submenu">
-                      <li><a href="blog.html">blog standard</a></li>
-                      <li><a href="blog-no-sidebar.html">blog no sidebar</a></li>
-                      <li><a href="blog-right-sidebar.html">blog right sidebar</a></li>
-                      <li><a href="blog-details.html">blog details 1</a></li>
-                      <li><a href="blog-details-2.html">blog details 2</a></li>
-                      <li><a href="blog-details-3.html">blog details 3</a></li>
-                    </ul>
+                    <a href="/coins/new">Add New Coin</a>
                   </li>
-                  <li><a href="about.html">About</a></li>
-                  <li><a href="contact.html">Contact</a></li>
+                  <li><a href="/about">About</a></li>
                 </ul>
               </nav>
             </div>
@@ -161,13 +70,11 @@
                 </div>
               </div>
               <div class="same-style account-satting">
-                <a class="account-satting-active" href="#"><i class="pe-7s-user-female"></i></a>
+                <a class="account-satting-active" href="/coins/new"><i class="pe-7s-user-female"></i></a>
                 <div class="account-dropdown">
                   <ul>
-                    <li><a href="/">Login</a></li>
-                    <li><a href="/">Login</a></li>
-                    <li><a href="wishlist.html">Wishlist</a></li>
-                    <li><a href="my-account.html">my account</a></li>
+                    <li><a href="/logout">Logout</a></li>
+                    <li><a href="/usercoins">My Coins</a></li>
                   </ul>
                 </div>
               </div>
@@ -202,18 +109,9 @@
                   <a href="/usercoins">My Coins</a>
                 </li>
                 <li>
-                  <a href="blog.html">Blog</a>
-                  <ul>
-                    <li><a href="blog.html">blog standard</a></li>
-                    <li><a href="blog-no-sidebar.html">blog no sidebar</a></li>
-                    <li><a href="blog-right-sidebar.html">blog right sidebar</a></li>
-                    <li><a href="blog-details.html">blog details 1</a></li>
-                    <li><a href="blog-details-2.html">blog details 2</a></li>
-                    <li><a href="blog-details-3.html">blog details 3</a></li>
-                  </ul>
+                  <a href="/coins/new">Add New Coin</a>
                 </li>
                 <li><a href="/about">About us</a></li>
-                <li><a href="contact.html">Contact</a></li>
               </ul>
             </nav>
           </div>
@@ -223,7 +121,7 @@
                 <li>
                   <a href="index.html">HOME</a>
                   <ul>
-                    <li>
+                    <!-- <li>
                       <a href="#">Demo Group 01</a>
                       <ul>
                         <li><a href="index.html">Home 1 – Fashion</a></li>
@@ -238,7 +136,7 @@
                         <li><a href="index-10.html">Home 10 – Furniture</a></li>
                         <li><a href="index-11.html">Home 11 - showcase slider</a></li>
                       </ul>
-                    </li>
+                    </li> -->
                     <li>
                       <a href="#">Demo Group 02</a>
                       <ul>
@@ -255,83 +153,36 @@
                         <li><a href="index-22.html">Home 22 – Handmade</a></li>
                       </ul>
                     </li>
-                    <li>
-                      <a href="#">Demo Group 03</a>
-                      <ul>
-                        <li><a href="index-23.html">Home 23 – Organic</a></li>
-                        <li><a href="index-24.html">Home 24 – Pet Food</a></li>
-                        <li><a href="index-25.html">Home 25 – Auto Parts</a></li>
-                        <li><a href="index-26.html">Home 26 – Cake Shop</a></li>
-                        <li><a href="index-27.html">Home 27 – Kids Fashion</a></li>
-                        <li><a href="index-28.html">Home 28 – Book Shop</a></li>
-                        <li><a href="index-29.html">Home 29 – Flower Shop</a></li>
-                        <li><a href="index-30.html">Home 30 – Instagram</a></li>
-                        <li><a href="index-31.html">Home 31 – Black Friday</a></li>
-                        <li><a href="index-32.html">Home 32 – Valentine Day</a></li>
-                        <li><a href="index-33.html">Home 33 – Medical Equipment</a></li>
-                      </ul>
-                    </li>
+                    <li></li>
                   </ul>
                 </li>
-                <li>
-                  <a href="shop.html">Shop</a>
-                  <ul>
-                    <li>
-                      <a href="#">shop layout</a>
-                      <ul>
-                        <li><a href="shop.html">standard style</a></li>
-                        <li><a href="shop-filter.html">Grid filter style</a></li>
-                        <li><a href="shop-grid-2-col.html">Grid 2 column</a></li>
-                        <li><a href="shop-no-sidebar.html">Grid No sidebar</a></li>
-                        <li><a href="shop-grid-fw.html">Grid full wide</a></li>
-                        <li><a href="shop-right-sidebar.html">Grid right sidebar</a></li>
-                        <li><a href="shop-list.html">list 1 column box</a></li>
-                        <li><a href="shop-list-fw.html">list 1 column full wide</a></li>
-                        <li><a href="shop-list-fw-2col.html">list 2 column full wide</a></li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a href="#">product details</a>
-                      <ul>
-                        <li><a href="product-details.html">tab style 1</a></li>
-                        <li><a href="product-details-2.html">tab style 2</a></li>
-                        <li><a href="product-details-3.html">tab style 3</a></li>
-                        <li><a href="product-details-4.html">sticky style</a></li>
-                        <li><a href="product-details-5.html">gallery style</a></li>
-                        <li><a href="product-details-slider-box.html">Slider style</a></li>
-                        <li><a href="product-details-affiliate.html">affiliate style</a></li>
-                        <li><a href="product-details-6.html">fixed image style</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
+                <li></li>
                 <li><a href="/coins">Coin Database</a></li>
                 <li>
                   <a href="#">Pages</a>
                   <ul>
-                    <li><a href="/abouts">about us</a></li>
-                    <li><a href="cart-page.html">cart page</a></li>
+                    <li><a href="/about">about us</a></li>
+                    <!-- <li><a href="cart-page.html">cart page</a></li>
                     <li><a href="checkout.html">checkout</a></li>
                     <li><a href="wishlist.html">wishlist</a></li>
-                    <li><a href="my-account.html">my account</a></li>
+                    <li><a href="my-account.html">my account</a></li> -->
                     <li><a href="/">login</a></li>
-                    <li><a href="contact.html">contact us</a></li>
+
                     <li><a href="404.html">404 page</a></li>
                   </ul>
                 </li>
                 <li>
-                  <a href="blog.html">Blog</a>
-                  <ul>
-                    <li><a href="blog.html">blog standard</a></li>
-                    <li><a href="blog-no-sidebar.html">blog no sidebar</a></li>
-                    <li><a href="blog-right-sidebar.html">blog right sidebar</a></li>
-                    <li><a href="blog-details.html">blog details 1</a></li>
-                    <li><a href="blog-details-2.html">blog details 2</a></li>
-                    <li><a href="blog-details-3.html">blog details 3</a></li>
-                  </ul>
+                  <a href="/coins/new">Add New Coin</a>
+                  <!-- <ul>
+                    <li><a href="/coins/new">coin standard</a></li>
+                    <li><a href="/coins/new">coin no sidebar</a></li>
+                    <li><a href="/coins/new">coin right sidebar</a></li>
+                    <li><a href="/coins/new">coin details 1</a></li>
+                    <li><a href="/coins/new">coin details 2</a></li>
+                    <li><a href="/coins/new">coin details 3</a></li>
+                  </ul> -->
                 </li>
                 <li><a href="/about">About us</a></li>
-                <li><a href="contact.html">Contact</a></li>
               </ul>
             </nav>
           </div>
@@ -342,7 +193,7 @@
       <!-- <router-link to="/">Home</router-link> -->
       <!-- | -->
       <!-- <router-link to="/about">About</router-link> -->
-      |
+      <!-- |
       <li v-if="!isLoggedIn()">
         <router-link to="/signup">Signup</router-link>
       </li>
@@ -362,7 +213,7 @@
 
       <li v-if="isAdmin()">
         <router-link to="/coins/new">Add New Coin</router-link>
-      </li>
+      </li> -->
     </div>
 
     <router-view />
@@ -373,7 +224,7 @@
             <div class="copyright mb-30">
               <div class="footer-logo">
                 <a href="/">
-                  <img alt="" src="assets/img/logo/logo.png" />
+                  <img alt="" src="assets/img/logo/logo.png " class="logo" />
                 </a>
               </div>
               <p>
@@ -1249,3 +1100,10 @@ export default {
   },
 };
 </script>
+<style>
+.logo {
+  width: 100px;
+  height: 100px;
+  margin: 0px !important;
+}
+</style>
--- src/views/CoinsIndex.vue
+++ src/views/CoinsIndex.vue
@@ -1,46 +1,185 @@
 <template>
   <div class="posts-index">
     <div class="card-deck">
-      <div class="card">
-        search:
-        <input v-model="descriptionFilter" />
+      <div class="mx-auto bg-warning" style="width: auto">
+        <!-- search:
+        <input v-model="descriptionFilter" /> -->
+        <div class="product-area pb-60">
+          <div class="container">
+            <div class="tab-filter-wrap mb-60">
+              <div class="product-tab-list-2 nav">
+                <a href="#product-1" data-bs-toggle="tab"></a>
+                <a class="active" href="#product-2" data-bs-toggle="tab">
+                  <h1>All Coins</h1>
+                </a>
+                <a href="coins" data-bs-toggle="tab"></a>
+              </div>
+              <div class="filter-active">
+                <a href="#">
+                  <i class="fa fa-plus"></i>
+                  filter
+                </a>
+              </div>
+            </div>
+            <div class="product-filter-wrapper">
+              <div class="row">
+                <!-- Product Filter -->
+                <div class="col-md-3 col-sm-6 col-xs-12 mb-30">
+                  <div class="product-filter">
+                    <h5>Sort by</h5>
+                    <ul class="sort-by">
+                      <li><a href="#">Default</a></li>
+                      <li><a href="#">Popularity</a></li>
+                      <li><a href="#">Average rating</a></li>
+                      <li><a href="#">Newness</a></li>
+                      <li><a href="#">Price: Low to High</a></li>
+                      <li><a href="#">Price: High to Low</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <!-- Product Filter -->
+                <div class="col-md-3 col-sm-6 col-xs-12 mb-30">
+                  <div class="product-filter">
+                    <h5>color filters</h5>
+                    <ul class="color-filter">
+                      <li>
+                        <input type="checkbox" value="" />
+                        <a href="#">Black</a>
+                      </li>
+                      <li>
+                        <input type="checkbox" value="" />
+                        <a href="#">Brown</a>
+                      </li>
+                      <li>
+                        <input type="checkbox" value="" />
+                        <a href="#">Orange</a>
+                      </li>
+                      <li>
+                        <input type="checkbox" value="" />
+                        <a href="#">red</a>
+                      </li>
+                      <li>
+                        <input type="checkbox" value="" />
+                        <a href="#">Yellow</a>
+                      </li>
+                      <li>
+                        <input type="checkbox" value="" />
+                        <a href="#">purple</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <!-- Product Filter -->
+                <div class="col-md-3 col-sm-6 col-xs-12 mb-30">
+                  <div class="product-filter">
+                    <h5>product tags</h5>
+                    <div class="product-tags">
+                      <ul>
+                        <li><a href="#">New</a></li>
+                        <li><a href="#">brand</a></li>
+                        <li><a href="#">black</a></li>
+                        <li><a href="#">white</a></li>
+                        <li><a href="#">chire</a></li>
+                        <li><a href="#">table</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <!-- Product Filter -->
+                <div class="col-md-3 col-sm-6 col-xs-12 mb-30">
+                  <div class="product-filter">
+                    <h5>Filter by price</h5>
+                    <div class="price-filter mt-25">
+                      <div class="price-slider-amount">
+                        <input type="text" id="amount" name="price" placeholder="Add Your Price" />
+                      </div>
+                      <div id="slider-range"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              v-for="coin in filterBy(coins, descriptionFilter, 'description')"
+              :key="coin.id"
+              v-bind:class="{ selected: coin === currentCoin }"
+              v-on:click="currentCoin = coin"
+            >
+              <div class="tab-content jump">
+                <div class="tab-pane" id="product-1">
+                  <div class="row"></div>
+                </div>
+                <div class="tab-pane active" id="product-2">
+                  <div class="row">
+                    <div class="row">
+                      <div class="col-xl-3 col-md-6 col-lg-4 col-sm-6">
+                        <div class="product-wrap-2 mb-25">
+                          <div class="product-img">
+                            <a v-bind:href="`/coins/${coin.id}`">
+                              <img class="default-img" v-bind:src="coin.image" alt="" />
+                              <img class="hover-img" v-bind:src="coin.image" alt="" />
+                            </a>
+                          </div>
+                          <div class="product-content-2">
+                            <div class="title-price-wrap-2">
+                              <h3>
+                                <p class="card-text">Denomination: {{ coin.denomination }}</p>
+                              </h3>
+                              <div class="price-2">
+                                <span class="card-text">Metal: {{ coin.metal }}</span>
+                                <span class="card-text">Mint: {{ coin.mint }}</span>
+                                <h5 class="card-title text-center">Description: {{ coin.description }}</h5>
+                              </div>
+                            </div>
+                            <div class="pro-wishlist-2">
+                              <a title="Wishlist" href="wishlist.html"><i class="fa fa-heart-o"></i></a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            v-for="coin in filterBy(coins, descriptionFilter, 'description')"
+            :key="coin.id"
+            v-bind:class="{ selected: coin === currentCoin }"
+            v-on:click="currentCoin = coin"
+          >
+            <div class="card" style="width: auto">
+              <div class="card-body">
+                <h5 class="card-title text-center">Description: {{ coin.description }}</h5>
+                <p class="card-text">Metal: {{ coin.metal }}</p>
+                <p class="card-text">Denomination: {{ coin.denomination }}</p>
+                <p class="card-text">Mint: {{ coin.mint }}</p>
+                <p class="card-text">Year: {{ coin.year }}</p>
+                <img v-bind:src="coin.image" />
 
-        <div
-          v-for="coin in filterBy(coins, descriptionFilter, 'description')"
-          :key="coin.id"
-          v-bind:class="{ selected: coin === currentCoin }"
-          v-on:click="currentCoin = coin"
-        >
-          <div class="col-3">
-            <div class="card-body">
-              <h5 class="card-title">Description: {{ coin.description }}</h5>
-              <p class="card-text">Metal: {{ coin.metal }}</p>
-              <p class="card-text">Denomination: {{ coin.denomination }}</p>
-              <p class="card-text">Mint: {{ coin.mint }}</p>
-              <p class="card-text">Year: {{ coin.year }}</p>
-              <img v-bind:src="coin.image" />
+                <router-link v-bind:to="`/coins/${coin.id}/edit`">
+                  <button v-if="isAdmin()">Edit coin</button>
+                </router-link>
+                <router-link v-bind:to="`usercoins`">
+                  <button v-on:click="addUserCoin(coin)">Add To My Coin</button>
+                </router-link>
 
-              <router-link v-bind:to="`/coins/${coin.id}/edit`">
-                <button v-if="isAdmin()">Edit coin</button>
-              </router-link>
-              <router-link v-bind:to="`usercoins`">
-                <button v-on:click="addUserCoin(coin)">Add To My Coin</button>
-              </router-link>
-
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <!-- <div v-for="coin in coins" :key="coin.id">
+      <!-- <div v-for="coin in coins" :key="coin.id">
       <router-link v-bind:to="`/coins/${coin.id}`">
         <h2>Title: {{ coin.title }}</h2>
         <p>Body: {{ coin.body }}</p>
         <img v-bind:src="coin.image" alt="coin.title" />
       </router-link>
     </div> -->
+    </div>
   </div>
 </template>
 <style>
@@ -49,7 +188,7 @@
 }
 .selected {
   color: rgb(255, 255, 255);
-  background-color: rgb(201, 152, 29);
+  background-color: rgb(68, 66, 60);
   transition: background-color 1.5s ease;
 }
 </style>
@@ -100,3 +239,13 @@ export default {
   },
 };
 </script>
+<style>
+.mx-auto {
+  margin-right: auto !important;
+  margin-left: auto !important;
+}
+.p {
+  text-align: right;
+  line-height: 100px;
+}
+</style>
